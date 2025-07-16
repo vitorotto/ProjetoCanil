e521f2e4b2dfa0a90bd1c3c5afd24536d7a965de
vitorotto <vitorotto05@gmail.com>
2025-07-16 20:01:35 -0300

    Fix: mustache digitado errado

--- src/server.ts
+++ src/server.ts
@@ -9,9 +9,9 @@ dotenv.config();
 
 const server = express();
 
-server.set('view engine', 'mustacheExpress');
+server.set('view engine', 'mustache');
 server.set('views', path.join(__dirname, 'views'));
-server.engine('mustacheExpress', mustacheExpress());
+server.engine('mustache', mustacheExpress());
 
 server.use(express.static(path.join(__dirname, '../public')));
 

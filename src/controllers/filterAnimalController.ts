b1389afb6cf5a549ce93b3b1b9621ab03981a8b8
vitorotto <vitorotto05@gmail.com>
2025-07-16 20:38:26 -0300

    Aula06: Ativando o menu

--- src/controllers/filterAnimalController.ts
+++ src/controllers/filterAnimalController.ts
@@ -1,8 +1,10 @@
-import { Express } from "express";
 import { Request, Response } from "express";
+
+import { createMenuObject } from "../helpers/createMenuObject";
 
 export const home = (req: Request, res: Response) => {
     res.render('pages/page', {
+        menu: createMenuObject('all'),
         banner: {
             title: 'Todos os animais',
             background: 'allanimals.jpg'
@@ -11,6 +13,7 @@ export const home = (req: Request, res: Response) => {
 }
 export const dogs = (req: Request, res: Response) => {
     res.render('pages/page', {
+        menu: createMenuObject('dog'),
         banner: {
             title: 'Todos os cachorros',
             background: 'banner_dog.jpg'
@@ -19,6 +22,7 @@ export const dogs = (req: Request, res: Response) => {
 }
 export const cats = (req: Request, res: Response) => {
     res.render('pages/page', {
+        menu: createMenuObject('cat'),
         banner: {
             title: 'Todos os gatos',
             background: 'banner_cat.jpg'
@@ -27,6 +31,7 @@ export const cats = (req: Request, res: Response) => {
 }
 export const fishes = (req: Request, res: Response) => {
     res.render('pages/page', {
+        menu: createMenuObject('fish'),
         banner: {
             title: 'Todos os peixes',
             background: 'banner_fish.jpg'
